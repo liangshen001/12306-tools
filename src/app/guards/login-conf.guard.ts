4bc23f056ccfa74b9935207744993fc5692bdbd5
wangliang <850598163@qq.com>
2020-01-07 20:14:51 +0800

    尝试 获取cookie

---
+++ src/app/guards/login-conf.guard.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {LoginConfApi} from '../services/api/login-conf.api';
+import {ApiService} from '../services/api.service';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginConfGuard implements CanActivateChild {
+
+    constructor(private loginConfApi: LoginConfApi,
+                private apiService: ApiService) {}
+
+    canActivateChild(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        debugger;
+        return this.apiService.request({
+            api: this.loginConfApi
+        }).pipe(
+            map(data => !!data)
+        );
+    }
+}
